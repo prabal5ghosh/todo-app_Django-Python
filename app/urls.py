517144d78922ce094b46c694b8592e6c2c9279fc
Prabal <prabal5ghosh@gmail.com>
2022-08-24 19:41:01 +0530

    new changes

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('add-todo/' , views.add_todo ),
+    path('delete-todo/<int:id>' , views.delete_todo ),
+    path('change-status/<int:id>/<str:status>' , views.change_todo ),
+    path('logout/' , views.signout ),
+]
